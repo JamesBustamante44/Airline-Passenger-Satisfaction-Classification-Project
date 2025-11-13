2780e36b738ccc2e4f57672de71d1cefc24974fd
JamesBustamante44 <james.bustamante44@gmail.com>
2025-11-13 00:16:58 -0500

    Fix indentation for number_input in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -171,7 +171,7 @@ with tab_predict:
                         default = int(df[feat].mean())
                         step = 1
                         fmt = '%d'
-                    data[feat] = st.number_input(feat, value=default, min_value=vmin, max_value=vmax, step=step, format=fmt)                
+                        data[feat] = st.number_input(feat, value=default, min_value=vmin, max_value=vmax, step=step, format=fmt)                
             else:
                 options = df[feat].dropna().unique().tolist()
                 if not options:
